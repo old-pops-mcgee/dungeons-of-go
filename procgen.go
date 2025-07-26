28b0ee0a3cac132ae1da65807287fb1ee18ee7e6
old-pops-mcgee <oldpopsmcgeegaming@gmail.com>
2025-07-26 17:57:54 -0400

    Adds tunnel building

--- procgen.go
+++ procgen.go
@@ -1,6 +1,11 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"math"
+	"math/rand/v2"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 type RectangularRoom struct {
 	gMap        *GameMap
@@ -20,8 +25,8 @@ func GetNewRectangularRoom(gm *GameMap, tl rl.Vector2, width int, height int) Re
 
 func (r *RectangularRoom) GetCenter() rl.Vector2 {
 	return rl.Vector2{
-		X: (r.TopLeft.X + r.BottomRight.X) / 2,
-		Y: (r.TopLeft.Y + r.BottomRight.Y) / 2,
+		X: float32(math.Floor(float64(r.TopLeft.X+r.BottomRight.X) / 2)),
+		Y: float32(math.Floor(float64(r.TopLeft.Y+r.BottomRight.Y) / 2)),
 	}
 }
 
@@ -36,24 +41,46 @@ func (r *RectangularRoom) GetInnerIndices() []int {
 }
 
 func GenerateTunnelIndices(g *Game, pointA rl.Vector2, pointB rl.Vector2) []int {
+	coords := []rl.Vector2{}
 	indices := []int{}
+	// Get the random corner so we can create an L-shaped tunnel
+	var corner rl.Vector2
+	if rand.Float32() < 0.5 {
+		// Move horizontally, then vertically
+		corner = rl.Vector2{X: pointB.X, Y: pointA.Y}
+	} else {
+		// Move vertically, then horizontally
+		corner = rl.Vector2{X: pointA.X, Y: pointB.Y}
+	}
 
-	// Determine
+	coords = append(coords, BresenhamLine(pointA, corner)...)
+	coords = append(coords, BresenhamLine(corner, pointB)...)
+
+	for _, coord := range coords {
+		indices = append(indices, g.gameMap.CoordToIndex(coord))
+	}
 
 	return indices
 }
 
 func GenerateDungeon(g *Game, mapWidth int, mapHeight int) GameMap {
 	gMap := NewGameMap(g, mapWidth, mapHeight)
+	g.gameMap = gMap
 
 	r1 := GetNewRectangularRoom(&gMap, rl.Vector2{X: 20, Y: 15}, 10, 15)
 	r2 := GetNewRectangularRoom(&gMap, rl.Vector2{X: 35, Y: 15}, 10, 15)
 
+	// Carve out rooms
 	for _, tileIndex := range r1.GetInnerIndices() {
 		gMap.Tiles[tileIndex] = Floor
 	}
 
 	for _, tileIndex := range r2.GetInnerIndices() {
+		gMap.Tiles[tileIndex] = Floor
+	}
+
+	// Carve out tunnel between rooms
+	for _, tileIndex := range GenerateTunnelIndices(g, r1.GetCenter(), r2.GetCenter()) {
 		gMap.Tiles[tileIndex] = Floor
 	}
 
