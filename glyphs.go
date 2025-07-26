17ff21e61efd35f42be9e9851ec59544d2ebf43d
old-pops-mcgee <oldpopsmcgeegaming@gmail.com>
2025-07-26 11:48:50 -0400

    Adds glyph concept

---
+++ glyphs.go
@@ -0,0 +1,10 @@
+package main
+
+type Glyph struct {
+	GX int // X position on the spritesheet
+	GY int // Y position on the spritesheet
+}
+
+var PlayerGlyph Glyph = Glyph{GX: 0, GY: 4} // @
+var FloorGlyph Glyph = Glyph{GX: 14, GY: 2} // .
+var WallGlyph Glyph = Glyph{GX: 3, GY: 2}   // #
