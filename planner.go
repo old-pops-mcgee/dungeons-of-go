7672e3355f50a7cc3ae3a06813e75370da98f70d
old-pops-mcgee <oldpopsmcgeegaming@gmail.com>
2025-08-09 12:12:29 -0400

    WIP planner changes

--- planner.go
+++ planner.go
@@ -1,33 +1,49 @@
 package main
 
 import (
-	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type Planner interface {
-	planMovementActionSet(e *Entity) map[MovementAction]bool
+	planNextAction(e *Entity) EntityAction
 }
 
 type PlayerPlanner struct {
 }
 
-func (p PlayerPlanner) planMovementActionSet(e *Entity) map[MovementAction]bool {
-	actionSet := map[MovementAction]bool{}
+func (p PlayerPlanner) planNextAction(e *Entity) EntityAction {
+	// Generate the movement action set
+	movementActionSet := map[MovementAction]bool{}
+	var movementDelta MovementDelta
 
 	for k, v := range e.movementActionSet {
-		actionSet[k] = v
+		movementActionSet[k] = v
 		delete(e.movementActionSet, k)
 	}
 
-	return actionSet
+	// Process movements from each movement action
+	for movement := range movementActionSet {
+		tempMovementDelta := MOVEMENT_DELTAS[movement]
+		movementDelta.dx += tempMovementDelta.dx
+		movementDelta.dy += tempMovementDelta.dy
+	}
+
+	// Clamp the movement deltas to ensure we don't process to big a step
+	movementDelta.dx = int(rl.Clamp(float32(movementDelta.dx), -1, 1))
+	movementDelta.dy = int(rl.Clamp(float32(movementDelta.dy), -1, 1))
+
+	// Find the target coordinates
+	targetCoords := e.drawableEntity.mapCoords
+
+	targetCoords.X += float32(movementDelta.dx)
+	targetCoords.Y += float32(movementDelta.dy)
+
+	return e.getEntityActionForTarget(targetCoords)
 }
 
 type HostileEnemyPlanner struct {
 }
 
-func (h HostileEnemyPlanner) planMovementActionSet(e *Entity) map[MovementAction]bool {
-	actionSet := map[MovementAction]bool{}
-	actionSet[MovementAction(Stand)] = true
-	fmt.Println("Contemplating actions")
-	return actionSet
+func (h HostileEnemyPlanner) planNextAction(e *Entity) EntityAction {
+	return Stand
 }
